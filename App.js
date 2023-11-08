4d9235eb9491dff91f1bc0c154254964d80e350f
Mesut Biskin <msbiskin@gmail.com>
2023-11-08 13:34:18 -0500

    Mobile App Created

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
+import PostCard from "./components/PostCard";
 
 export default function App() {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  // useEffect is being called on different lifecyle of the component
+  // 1. When the component mounts
+  // 2. when the component upMounts
+
+  // How to get a component to re-render
+  // 1. change state variable
+  // 2. change props
+
+  const handleFetchData = () => {
+    console.log("fetching data");
+
+    fetch("http://192.168.10.82:8080", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => setBlogPosts(data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    fetch("http://192.168.10.82:8080") 
+      .then((res) => res.json())
+      .then((data) => setBlogPosts(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView >
       <StatusBar style="auto" />
-    </View>
+      <View>
+        <ScrollView>
+          {blogPosts.map((singlePost, index) => {
+            // single post is an object within an array
+
+            return <PostCard singlePost={singlePost} index={index} key={singlePost._id} />;
+          })}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/PostCard.js
@@ -0,0 +1,54 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+
+export default function PostCard({singlePost, index}) {
+   
+  return (
+    <View style={styles.cardContainer}>
+      <View style={styles.headerContainer}>
+        <Image
+          style={styles.avatarImage}
+          source={{
+            uri: `https://randomuser.me/api/portraits/men/${index}.jpg`,
+          }}
+        />
+        <Text> {singlePost.title}</Text>
+      </View>
+      <Image
+        style={styles.image}
+        source={{ uri: `https://source.unsplash.com/random/${index}` }} />
+      <Text>{singlePost.content}</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardContainer: {
+    marginVertical: 20,
+    paddingVertical: 20,
+    borderBottomColor: "#999",
+    borderBottomWidth: 1,
+  },
+
+  headerContainer: {
+    flexDirection: "row",
+    marginHorizontal: 20,
+  },
+  avatarImage: {
+    borderRadius: 30,
+    marginRight: 10,
+    width: 50,
+    height: 50,
+  },
+
+  image: {
+    flex: 1,
+    aspectRatio: 1,
+    width: "100%",
+    marginVertical: 20,
+  },
+});
